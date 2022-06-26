f1851c4450c1bcd8b939a6294a4644ca7452eba1
Maciej Dąbrowski <dabrowski.mac.1@gmail.com>
2022-06-26 15:18:49 +0200

    ran cleanup-react-app

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders cleanup react app message', () => {
+  const { getByText } = render(<App />);
+  const cleanupMessage = getByText(/Cleanedup React App/i);
+  expect(cleanupMessage).toBeInTheDocument();
+});
